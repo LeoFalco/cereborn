08df01334a9fc5225a8ff3641b8a1a9cc7c9134b
Leo Falco <leonardo.falco@outlook.com>
2023-05-25 18:57:51 -0300

    feat: table preview and clipboard copy

--- src/app/home/form/form.component.ts
+++ src/app/home/form/form.component.ts
@@ -5,47 +5,98 @@ import { ref } from 'firebase/storage';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+  styleUrls: ['./form.component.scss'],
 })
 export class FormComponent {
-  public imageBefore: { base64: string, file: File } | null = null;
-  public imageAfter: { base64: string, file: File } | null = null;
+  private storage = inject(Storage);
+  private auth = inject(Auth);
 
-  private storage = inject(Storage)
-  private auth = inject(Auth)
+  private placeholder = 'https://via.placeholder.com/500';
 
+  public imageBefore: { file: File } | null = null;
+  public imageAfter: { file: File } | null = null;
+
+  public contentString = ""
 
   async uploadImages(event: MouseEvent) {
+    if (!this.auth.currentUser) return;
+    if (!this.imageBefore || !this.imageAfter) return;
+
     const button = event.target as HTMLButtonElement;
-
-    if (!this.auth.currentUser) return
-
-    if (!this.imageBefore || !this.imageAfter) return
-
-    const now = new Date().toISOString().replace('Z', '').replace(/\D/g, '')
-
     try {
       button.disabled = true;
-      if (this.imageBefore) {
+      button.innerText = 'Enviando...';
 
-        const storageRef = ref(this.storage, `/users/${this.auth.currentUser?.uid}/images/${now}/before/${this.imageBefore.file.name}`)
-        await uploadBytes(storageRef, this.imageBefore.file)
-        const downloadUrl = await getDownloadURL(storageRef)
-        console.log('downloadUrl', downloadUrl)
-      }
+      const now = new Date().toISOString().replace('Z', '').replace(/\D/g, '');
 
-      if (this.imageAfter) {
-        const storageRef = ref(this.storage, `/users/${this.auth.currentUser?.uid}/images/${now}/after/${this.imageAfter.file.name}`)
-        await uploadBytes(storageRef, this.imageAfter.file)
-        const downloadUrl = await getDownloadURL(storageRef)
-        console.log('downloadUrl', downloadUrl)
+      const beforeDownloadUrl = this.imageBefore
+        ? await this.upload({
+          file: this.imageBefore.file,
+          path: `/users/${this.auth.currentUser?.uid}/images/${now}/before/${this.imageBefore.file.name}`,
+        })
+        : null;
 
-      }
+      const afterDownloadUrl = this.imageAfter
+        ? await this.upload({
+          file: this.imageAfter.file,
+          path: `/users/${this.auth.currentUser?.uid}/images/${now}/after/${this.imageAfter.file.name}`,
+        })
+        : null;
 
+      this.contentString = stripIndent(`
+        <table>
+          <thead>
+            <tr>
+              <th>Antes</th>
+              <th>Depois</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                <!-- Antes -->
+                <img src="${beforeDownloadUrl || this.placeholder}">
+              </td>
+              <td>
+                <!-- Depois -->
+                <img src="${afterDownloadUrl || this.placeholder}">
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      `)
 
     } finally {
       button.disabled = false;
+      button.innerText = 'Enviar Imagens';
     }
+  }
+
+  async upload({ file, path }: { file: File; path: string }): Promise<string> {
+    const storageRef = ref(this.storage, path);
+    await uploadBytes(storageRef, file);
+    return getDownloadURL(storageRef);
+  }
+
+  async copy(event: MouseEvent){
+
+    const button = event.target as HTMLButtonElement;
+
+    // copy to clipboard
+    await navigator.clipboard.writeText(this.contentString);
+
+    button.innerText = 'Copiado!';
+    button.disabled = true;
+
+    setTimeout(() => {
+      button.innerText = 'Copiar';
+      button.disabled = false;
+    }, 1000)
+
 
   }
+}
+
+function stripIndent(template: string) {
+  return template.split('\n').map((line) => line.replace('        ', '')).join('\n').trim();
 }
